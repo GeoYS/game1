c78c7092fe1239b7eb92db96822ec5ed5d15c115
George Shen <george.school303@gmail.com>
2017-09-11 23:48:48 -0400

    Issue #2
    - Included some test tilemap files (made in Tiled)
    - draws tilemap from test.json
    - extracts object (tree) coords from object layer and draws them

---
+++ client/assets/ts_ground.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="ground" tilewidth="64" tileheight="64" tilecount="2" columns="2">
+ <image source="ts_ground.png" width="128" height="64"/>
+</tileset>
--- client/js/screens.js
+++ client/js/screens.js
@@ -91,18 +91,33 @@ var ingame = {
     init: function() {        
         game.stage.backgroundColor = '#454645';
         
+        map = game.add.tilemap('map');
+        map.addTilesetImage('ground', 'groundts');
+        layer = map.createLayer('groundlayer');
+        layer.resizeWorld();
+        
+        trees = game.add.group();
+        trees.enableBody = true;
+
+        map.createFromObjects('trees', 'tree', 'tree', 0, true, false, trees);
+        for (var i = 0; i < trees.children.length; i++) {
+            trees.children[i].body.setSize(64, 64, 0, 64);
+        }
+
         game.physics.startSystem(Phaser.Physics.ARCADE);
         game.physics.arcade.gravity.y = 0;
 
         player = game.add.sprite(0, 0, 'placeholder');
         game.physics.arcade.enable(player);    
-        player.containsPoint = function(x, y) {
-            if (this.x < x && x < this.x + this.width &&
-                this.y < y && y < this.y + this.height) {
+        player.containsPoint = function(coord) {
+            if (this.x < coord.x && coord.x < this.x + this.width &&
+                this.y < coord.y && coord.y < this.y + this.height) {
                 return true;
             }
             return false;
         }
+        
+        game.camera.follow(player);
         
         game.input.mouse.capture = true;    
         //game.input.onTap.add(onTap, this);
@@ -150,9 +165,14 @@ var ingame = {
         game.debug.text("Target x: " + (target ? target.x : 0) + " y: " + (target ? target.y : 0), 300, 204);
         game.debug.text("Left button down: " + activePointer.leftButton.isDown, 300, 222);
         game.debug.text("StartDrag x: " + (startDrag ? startDrag.x : 0) + " y: " + (startDrag ? startDrag.y : 0), 300, 240);
+        game.debug.body(player);
+        for (var i = 0; i < trees.children.length; i++) {
+            game.debug.body(trees.children[i]);
+        }
     }
 }
 
+var trees;
 var state = {playersOnline: 0};
 var sessionId = guid();
 
@@ -164,15 +184,19 @@ var startDrag = null;
 var target = null;
 var vel = 200;
 
+function screenToWorld(sx, sy) {
+    var worldCoords = {x: sx + game.camera.x, y: sy + game.camera.y};
+    return worldCoords;
+}
 
 function onTap(pointer, doubleTap) {
     if (!player.selected) {
-        if (player.containsPoint(pointer.x, pointer.y)) {
+        if (player.containsPoint(screenToWorld(pointer.x, pointer.y))) {
             player.selected = true;
             player.loadTexture('placeholder2');
         }
     } else {
-        if (!(player.containsPoint(pointer.x, pointer.y))) {
+        if (!(player.containsPoint(screenToWorld(pointer.x, pointer.y)))) {
             player.selected = false;
             player.loadTexture('placeholder');
         } else {
@@ -185,8 +209,8 @@ function onTap(pointer, doubleTap) {
 
 function onDrag(sX, sY, eX,eY) {
     if (player.selected) {
-        if (player.containsPoint(sX, sY)) {
-            target = {x: eX, y: eY};
+        if (player.containsPoint(screenToWorld(sX, sY))) {
+            target = screenToWorld(eX, eY);
             var angle = game.math.angleBetweenPoints(new Phaser.Point(sX, sY), new Phaser.Point(eX, eY));
             
             player.body.velocity.x = Math.cos(angle) * vel;
