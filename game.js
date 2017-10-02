83f19fb3c4c87840a8c41f59bf95f97e9eba86c2
George Shen <george.school303@gmail.com>
2017-10-02 01:42:53 -0400

    Issue #8 Added game manager abstraction layer

--- game.js
+++ game.js
@@ -1,27 +1,23 @@
+var util = require('./util.js')
+
 /*
  * A Game represents an individual match. Several Games can be happening at once.
  * Contains all the logic, including unit updating, applying user actions, etc.
  *
  */ 
 function Game(width, height) {
-    var units = [];
+    this.units = [];
     
     this.width = width;
     this.height = height;
+    this.isFinished = false;
     
     this.update = function(delta) {
         units.forEach(function(unit, i){
-            unit.update(unit);
+            unit.update(delta);
         });
-    };
-    
-    /*
-     * Handle user action. E.g. user moving a unit, using a card,
-     * client request to update game info, etc.
-     * Type of actione should be specified by action.type
-     */
-    this.handleUserAction = function (action) {
         
+        util.removeDead(units, 'isDead');
     };
     
     this.addUnit = function(unit) {
---
+++ util.js
@@ -0,0 +1,7 @@
+this.removeDead = function(list, deadKey) {    
+    for (let i = 0; i < list.length; i++) {
+        if (list[i][deadKey) {
+            list.splice(i, 1);                
+        }
+    }
+}
